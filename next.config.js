f8e34d4c87b9b15c20e3ef4c9254a1fb87f19d65
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2024-09-08 11:24:07 -0500

    stash

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // React Strict Mode can help identify potential issues but is not required for DevTools.
+  eslint: {
+    dirs: ['pages', 'components', 'lib'], // Run ESLint on specified directories during development
+  },
+};
 
 module.exports = nextConfig;
